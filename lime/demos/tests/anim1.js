ea533a5eb6de0726288d53b7239db8d0ff68d774
camperPZ <linhcao1152000@gmail.com>
2017-06-18 10:37:24 +0700

    Update anim1.js

--- lime/demos/tests/anim1.js
+++ lime/demos/tests/anim1.js
@@ -33,7 +33,7 @@ test.start = function() {
 
 	var menuscene = new lime.Scene;
 
-	var layer = (new lime.Layer).setPosition(200, 10);
+	var layer = (new lime.Layer).setPosition(200, 1000);
 	menuscene.appendChild(layer);
 
 
